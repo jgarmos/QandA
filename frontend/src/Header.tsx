c8b0e03d2f34d98b1a082f9375695aa243029ddd
jgarmos <javiergarciaramos@gmail.com>
2022-08-20 14:57:52 +0100

    Added header component

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { UserIcon } from './Icons';
+
+export const Header = () => (
+  <div>
+    <a href="./"> Q & A</a>
+    <input type="text" placeholder="Search"></input>
+
+    <a href="./signin">
+      <UserIcon />
+      <span>Sign In</span>
+    </a>
+  </div>
+);
