8af17eb3397ecccb5321d764c680ab812e7878ec
jgarmos <javiergarciaramos@gmail.com>
2022-09-04 16:47:55 +0100

    Styled components

--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,4 +1,14 @@
-import React from 'react';
-import user from './user.svg';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import user from './user.svg'
 
-export const UserIcon = () => <img src={user} alt="user" width="12px"></img>;
+export const UserIcon = () => (
+  <img
+    src={user}
+    alt="user"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+  ></img>
+);
