0972d471e7e69bf28347888b1b30808086424df5
jgarmos <javiergarciaramos@gmail.com>
2022-09-22 07:35:44 +0100

    Register IDataRepository in the Dependency injection

--- backend/QandA/Startup.cs
+++ backend/QandA/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DbUp;
+using QandA.Data;
 
 namespace QandA
 {
@@ -41,6 +37,9 @@ namespace QandA
             //Scripts
 
             services.AddRazorPages();
+
+            //register classes for depnedency injection
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
