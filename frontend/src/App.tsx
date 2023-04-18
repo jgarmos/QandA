0bff70cd3f4d5f148f8c29374fe77289c697cb1b
jgarmos <javiergarciaramos@gmail.com>
2023-04-18 11:20:45 +0100

    added lazyLoading for Ask page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { Component } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { css } from '@emotion/react';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
+
+const AskPage = lazy(() => import('./AskPage'));
 
 const App: React.FC = () => {
   return (
@@ -26,10 +27,28 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/ask" element={<AskPage />} />
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
+          <Route
+            path="ask"
+            element={
+              <Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { Page } from './Page';
 
 export const AskPage = () => <Page title="Ask a Question">{null}</Page>;
+export default AskPage;
